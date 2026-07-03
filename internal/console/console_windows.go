a79a7b51a2b18ba866444ce914a676f1677c9459
Md. Iftakhar Awal Chowdhury <iftakhar.awal@gmail.com>
2026-07-03 12:29:43 +0600

    refactor: rename constant and update permissions

--- internal/console/console_windows.go
+++ internal/console/console_windows.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-const attachParentProcess = ^uint32(0) // (DWORD)-1
+const ATTACH_PARENT_PROCESS = ^uint32(0) // (DWORD)-1
 
 var (
 	kernel32DLL       = windows.NewLazySystemDLL("kernel32.dll")
@@ -15,13 +15,13 @@ var (
 )
 
 func attachConsole() {
-	r1, _, _ := attachConsoleProc.Call(uintptr(attachParentProcess))
+	r1, _, _ := attachConsoleProc.Call(uintptr(ATTACH_PARENT_PROCESS))
 	if r1 == 0 {
 		// No parent console or failed to attach
 		return
 	}
 
-	if f, err := os.OpenFile("CONOUT$", os.O_RDWR, 0o644); err == nil {
+	if f, err := os.OpenFile("CONOUT$", os.O_RDWR, 0); err == nil {
 		os.Stdout = f
 		os.Stderr = f
 	}
