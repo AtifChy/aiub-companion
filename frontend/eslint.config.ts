76462fc854aef1b0e88c6bb5ca0edf6cf6a07774
Md. Iftakhar Awal Chowdhury <iftakhar.awal@gmail.com>
2026-07-12 12:05:16 +0600

    chore(lint): switch back to `eslint`
    `oxlint` ecosystem is not ready yet for the migration.
    It was a hassle getting things running in `oxlint`. So, we are switching back to `eslint` for now.

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactYouMightNotNeedAnEffect from "eslint-plugin-react-you-might-not-need-an-effect";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["dist/**", "node_modules/**", "bindings/**", "src/components/ui/**/*{.ts,.tsx}"]),
+  {
+    files: ["**/*{.ts,.tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      reactYouMightNotNeedAnEffect.configs.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+]);
