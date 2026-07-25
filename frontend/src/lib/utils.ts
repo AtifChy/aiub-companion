534c944cafc9c7194cf40903600884e746f633a6
Md. Iftakhar Awal Chowdhury <iftakhar.awal@gmail.com>
2026-07-25 21:57:55 +0600

    feat: add title case utility function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function titleCase(str: string) {
+  return str
+    .toLowerCase()
+    .split(/\s+/)
+    .map((word, index, arr) => {
+      if (index !== 0 && index !== arr.length - 1 && word.length <= 3) {
+        return word;
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}
